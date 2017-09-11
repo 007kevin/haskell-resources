d4eaabf992a41ab84fbb604420195c29c3c16463
kevin <kevin>
2017-09-10 22:19:40 -0400

    parser

--- write_yourself_a_scheme/02-Parsing/parser.hs
+++ write_yourself_a_scheme/02-Parsing/parser.hs
@@ -22,6 +22,20 @@ readExpr :: String -> String
 readExpr input = case parse (spaces >> symbol) "lisp" input of 
   Left err -> "No match: " ++ show err
   Right val -> "Found value"
+
+data ListVal = Atom String
+             | List [List Val]
+             | DottedList [ListVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
                    
 main :: IO ()
 main = do
